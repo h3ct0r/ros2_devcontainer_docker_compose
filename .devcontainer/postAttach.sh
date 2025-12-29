6ad2aaacbcc5951686c81ed68e62921525f1d989
h3ct0r <h3ct0r.ml@gmail.com>
2025-12-29 20:07:49 -0300

    adding specific parameters and configs for arm64 (tested on rpi5)

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -7,5 +7,5 @@
 
 # touch postattach_script_executed_$(date '+%Y-%m-%d_%H-%M-%S')
 
-find /tmp/.X11-unix/ -maxdepth 1 -type f -name '*' -delete
-rm /tmp/.X11-unix/X2
+find /tmp/.X11-unix/ -maxdepth 1 -type f -name '*' -delete || true
+rm /tmp/.X11-unix/X2 || true
